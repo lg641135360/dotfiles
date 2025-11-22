f5e309339c69fb5c97c8abe7210d0569405f683f
gang.li02 <gang.li02@mthreads.com>
2025-11-23 00:14:34 +0800

    add aarch64 awesome config
    Signed-off-by: gang.li02 <gang.li02@mthreads.com>

---
+++ .config/linux/awesome/autostart/ubuntu_aarch64.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# ==============================================
+# AwesomeWM autostart script
+# ==============================================
+
+run() {
+    if ! pgrep -x "$1" >/dev/null; then
+        "$@" &
+    fi
+}
+
+# Wait for X11 to be ready
+sleep 1
+
+xrdb merge ~/.Xresources
+xrandr --output eDP-1 --mode 0x4b
+# set touchpad natutral scrolling
+xinput set-prop 10 "libinput Natural Scrolling Enabled" 1
+xinput set-prop 10 "libinput Tapping Enabled" 1
+xinput set-prop 10 "libinput Click Method Enabled" 1 0
+xinput set-prop 10 "libinput Disable While Typing Enabled" 1
+
+run feh --bg-fill --randomize /usr/share/backgrounds/*
+run nm-applet
+run blueman-applet
+run pasystray
+run picom
+run fcitx5
+run redshift -l 30.6:114.3 -t 6500:4000
+run pot
+run dunst
+run udiskie -t
--- install.sh
+++ install.sh
@@ -243,6 +243,9 @@ ubuntu_aarch64_configs=(
     "command -v xmobar|.config/linux/xmobar/xmobarrc-ubuntu-aarch64|~/.config/xmobar/xmobarrc|Xmobar"
     "command -v dunst|.config/linux/dunst/dunstrc-ubuntu-aarch64|~/.config/dunst/dunstrc|Dunst"
     "command -v picom|.config/linux/picom/picom-lower-power.conf|~/.config/picom/picom.conf|Picom"
+    "command -v xrdb|.config/linux/x11/xresources/ubuntu_aarch64|~/.Xresources|Xresources"
+    "command -v awesome|.config/linux/awesome/rc/ubuntu_aarch64.lua|~/.config/awesome/rc.lua|AwesomeWM rc.lua"
+    "command -v awesome|.config/linux/awesome/autostart/ubuntu_aarch64.sh|~/.config/awesome/autostart.sh|AwesomeWM autostart script"
 )
 
 # Ubuntu amd64 (x86_64)
